93f890318f802597c4a8e851dfd24f7e491807ef
stepka reserva <stepka@reserva.com>
2025-07-31 12:52:19 +0200

    infra for wave execution

---
+++ nodeman/internal/infra/waveexec/waveexec_test.go
@@ -0,0 +1,140 @@
+package waveexec
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWaveExec(t *testing.T) {
+	fnCallsCount := 0
+
+	fn := func(ctx context.Context) (interface{}, error) {
+		time.Sleep(1 * time.Second)
+		fnCallsCount++
+		return nil, nil
+	}
+
+	waveExec := NewWaveExecutor(fn)
+	defer waveExec.Close()
+
+	// run 100 calls at the same time, fnCallsCount expected to be
+	// 1 (all calls in first wave) or 2 (some in the first wave, others in second)
+	var wg sync.WaitGroup
+	for range 100 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := waveExec.Invoke(context.Background())
+			require.NoError(t, err)
+		}()
+	}
+
+	wg.Wait()
+	require.Greater(t, fnCallsCount, 0)
+	require.LessOrEqual(t, fnCallsCount, 2)
+}
+
+func TestWaveExec_Cancels(t *testing.T) {
+	fnCallsCount := 0
+
+	fn := func(ctx context.Context) (interface{}, error) {
+		time.Sleep(1 * time.Second)
+		fnCallsCount++
+		return nil, nil
+	}
+
+	waveExec := NewWaveExecutor(fn)
+	defer waveExec.Close()
+
+	// run 100 calls with different timeouts
+	var wg sync.WaitGroup
+	for range 50 {
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+			defer cancel()
+			_, err := waveExec.Invoke(ctx)
+			require.Error(t, err)
+		}()
+		go func() {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), 2500*time.Millisecond)
+			defer cancel()
+			_, err := waveExec.Invoke(ctx)
+			require.NoError(t, err)
+		}()
+	}
+
+	wg.Wait()
+	require.Greater(t, fnCallsCount, 0)
+	require.LessOrEqual(t, fnCallsCount, 2)
+}
+
+func TestWaveExec_EarlyClose(t *testing.T) {
+	fnCallsCount := 0
+
+	fn := func(ctx context.Context) (interface{}, error) {
+		time.Sleep(1 * time.Second)
+		fnCallsCount++
+		return nil, nil
+	}
+
+	waveExec := NewWaveExecutor(fn)
+
+	// run 100 calls with early timeouts
+	var wg sync.WaitGroup
+	for range 100 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+			defer cancel()
+			_, err := waveExec.Invoke(ctx)
+			require.Error(t, err)
+		}()
+	}
+
+	wg.Wait()
+	waveExec.Close()
+
+	require.Greater(t, fnCallsCount, 0)
+	require.LessOrEqual(t, fnCallsCount, 2)
+}
+
+func TestWaveExec_Cancel(t *testing.T) {
+	fnCallsCount := 0
+	fn := func(ctx context.Context) (interface{}, error) {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-time.NewTimer(1 * time.Second).C:
+			fnCallsCount++
+			return nil, nil
+		}
+	}
+
+	waveExec := NewWaveExecutor(fn)
+
+	// run 100 calls with early timeouts
+	var wg sync.WaitGroup
+	for range 100 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+			defer cancel()
+			_, err := waveExec.Invoke(ctx)
+			require.Error(t, err)
+		}()
+	}
+
+	wg.Wait()
+	waveExec.Close()
+
+	require.Equal(t, fnCallsCount, 0)
+}
