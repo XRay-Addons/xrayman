d8b49c679b4aee01c88125d5835a2bed3c8734c9
stepka reserva <stepka@reserva.com>
2026-04-17 11:50:20 +0200

    admpage folder init

---
+++ nodeman/web/admpage/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+
+export default defineConfig({
+  integrations: [vue()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  },
+});
