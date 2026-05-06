0bef06c2486bc4dbd9d738df3db73668a71fb4fb
stepka reserva <stepka@reserva.com>
2026-05-06 15:21:54 +0200

    chaotic good init order

--- nodeman/internal/app/app.go
+++ nodeman/internal/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"context"
 	"errors"
+	"net/http"
 
 	client "github.com/XRay-Addons/xrayman/nodeman/internal/clients/node"
 	"github.com/XRay-Addons/xrayman/nodeman/internal/config"
@@ -45,134 +46,39 @@ func New(cfg config.Config, log *zap.Logger) (app *App, err error) {
 		}
 	}()
 
+	app = &App{
+		base: baseApp,
+	}
+
 	///////////////////////////////////////////////////////////////////////////
-	// create app components
+	// create app components - chaotic good init order
 
-	// db
-	db, err := sqldb.New(cfg.DBConn)
-	if err != nil {
-		return
-	}
-	baseApp.AddCloser(func(context.Context) error {
-		db.Close()
-		return nil
-	})
-
-	// storage
-	storage, err := dbstorage.New(context.TODO(), db)
+	// infrasturcture
+	infra, err := app.initInfra(cfg)
 	if err != nil {
 		return
 	}
 
-	// nodes http client
-	nodesClient := httpclient.NewClientFactory()
-	baseApp.AddCloser(func(context.Context) error {
-		nodesClient.Close()
-		return nil
-	})
-
-	// pool client
-	poolClient, err := client.NewPoolClient(
-		client.WithHTTPClient(nodesClient))
-	if err != nil {
-		return
-	}
-
-	// pool syncer
-	poolSyncer, err := poolsync.New(
-		poolClient, storage.PoolSyncStorage())
+	// pool sync
+	poolSyncer, err := app.initPoolSyncer(*infra)
 	if err != nil {
 		return
 	}
 
 	// password
-	pwd, err := password.New(storage.PasswordStorage())
+	pwd, err := password.New(infra.storage.PasswordStorage())
 	if err != nil {
 		return
 	}
 
-	// JWT
-	authJWT, err := jwt.New(cfg.JWTSecret)
-	if err != nil {
-		return
-	}
-
-	// nodes service
-	nodesService, err := nodes.New(
-		poolSyncer, storage.NodesStorage())
-	if err != nil {
-		return
-	}
-
-	// users service
-	usersService, err := users.New(
-		poolSyncer, storage.UsersStorage())
-	if err != nil {
-		return
-	}
-
-	// subscr service
-	subscrService, err := subscr.New(
-		storage.SubscrStorage(), subscr.WithLogger(log))
-	if err != nil {
-		return
-	}
-
-	// auth service
-	authService, err := auth.New(pwd, authJWT)
-	if err != nil {
-		return
-	}
-
-	// requests handler
-	requestsHandler, err := handler.New(
-		usersService,
-		nodesService,
-		subscrService,
-		authService,
-		handler.WithLogger(log))
-	if err != nil {
-		return
-	}
-
-	// security handler
-	securityHandler, err := security.New(authJWT)
-	if err != nil {
-		return
-	}
-
-	// api handler
-	apiHandler, err := api.NewHandler(
-		requestsHandler, securityHandler)
-	if err != nil {
-		return
-	}
-
-	// userpage spa
-	userpageSpa, err := pages.NewUserPage(
-		cfg.APIPrefix, cfg.UserSpaPrefix)
-	if err != nil {
-		return
-	}
-	// admpage spa
-	admpageSpa, err := pages.NewAdmPage(
-		cfg.APIPrefix, cfg.AdminSpaPrefix, cfg.UserSpaPrefix)
-	if err != nil {
-		return
-	}
-
-	// router
-	r, err := router.New(
-		router.WithHandler(cfg.APIPrefix, apiHandler),
-		router.WithSPA(cfg.UserSpaPrefix, userpageSpa),
-		router.WithSPA(cfg.AdminSpaPrefix, admpageSpa),
-		router.WithLogger(log))
+	// services
+	services, err := app.initServices(poolSyncer, pwd, infra.authJWT, infra.storage, log)
 	if err != nil {
 		return
 	}
 
 	// http server
-	httpServer, err := server.New(cfg.Endpoint, r)
+	httpServer, err := app.initHttpServer(cfg, *services, infra.authJWT, log)
 	if err != nil {
 		return
 	}
@@ -188,7 +94,7 @@ func New(cfg config.Config, log *zap.Logger) (app *App, err error) {
 
 	// migrate db
 	baseApp.AddBootstrap("migrated db", func(ctx context.Context) error {
-		return storage.Migrage(ctx, dbstorage.WithLogger(log))
+		return infra.storage.Migrage(ctx, dbstorage.WithLogger(log))
 	}, func(err error) bool {
 		return errors.Is(err, errdefs.ErrTemporaryUnavailable)
 	})
@@ -231,6 +137,167 @@ func New(cfg config.Config, log *zap.Logger) (app *App, err error) {
 	return &App{
 		base: baseApp,
 	}, nil
+}
+
+type infrasturcture struct {
+	storage *dbstorage.Storage
+	authJWT *jwt.JWT
+}
+
+func (a *App) initInfra(cfg config.Config) (infra *infrasturcture, err error) {
+	infra = &infrasturcture{}
+
+	// db
+	db, err := sqldb.New(cfg.DBConn)
+	if err != nil {
+		return
+	}
+	a.base.AddCloser(func(context.Context) error {
+		db.Close()
+		return nil
+	})
+
+	// storage
+	if infra.storage, err = dbstorage.New(context.TODO(), db); err != nil {
+		return
+	}
+
+	// JWT
+	if infra.authJWT, err = jwt.New(cfg.JWTSecret); err != nil {
+		return
+	}
+	return infra, nil
+}
+
+func (a *App) initPoolSyncer(infra infrasturcture) (ps poolsync.Syncer, err error) {
+	// nodes http client
+	nc := httpclient.NewClientFactory()
+	a.base.AddCloser(func(context.Context) error {
+		nc.Close()
+		return nil
+	})
+
+	// pool client
+	pc, err := client.NewPoolClient(client.WithHTTPClient(nc))
+	if err != nil {
+		return
+	}
+
+	// pool syncer
+	ps, err = poolsync.New(pc, infra.storage.PoolSyncStorage())
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+type services struct {
+	nodes  *nodes.Service
+	users  *users.Service
+	subscr *subscr.Service
+	auth   *auth.Service
+}
+
+func (a *App) initServices(
+	ps poolsync.Syncer,
+	pwd *password.Password,
+	authJWT *jwt.JWT,
+	s *dbstorage.Storage,
+	log *zap.Logger,
+) (ss *services, err error) {
+	ss = &services{}
+
+	// nodes service
+	if ss.nodes, err = nodes.New(ps, s.NodesStorage()); err != nil {
+		return
+	}
+
+	// users service
+	if ss.users, err = users.New(ps, s.UsersStorage()); err != nil {
+		return
+	}
+
+	// subscr service
+	if ss.subscr, err = subscr.New(s.SubscrStorage(), subscr.WithLogger(log)); err != nil {
+		return
+	}
+
+	// auth service
+	if ss.auth, err = auth.New(pwd, authJWT); err != nil {
+		return
+	}
+
+	return
+}
+
+func (a *App) initHttpServer(
+	cfg config.Config,
+	s services,
+	authJWT *jwt.JWT,
+	log *zap.Logger,
+) (h *server.HttpServer, err error) {
+	// api handler
+	apiHandler, err := a.initHandler(s, authJWT, log)
+	if err != nil {
+		return
+	}
+
+	// userpage spa
+	userpageSpa, err := pages.NewUserPage(
+		cfg.APIPrefix, cfg.UserSpaPrefix)
+	if err != nil {
+		return
+	}
+
+	// admpage spa
+	admpageSpa, err := pages.NewAdmPage(
+		cfg.APIPrefix, cfg.AdminSpaPrefix, cfg.UserSpaPrefix)
+	if err != nil {
+		return
+	}
+
+	// router
+	r, err := router.New(
+		router.WithHandler(cfg.APIPrefix, apiHandler),
+		router.WithSPA(cfg.UserSpaPrefix, userpageSpa),
+		router.WithSPA(cfg.AdminSpaPrefix, admpageSpa),
+		router.WithLogger(log))
+	if err != nil {
+		return
+	}
+
+	// http server
+	if h, err = server.New(cfg.Endpoint, r); err != nil {
+		return
+	}
+
+	return
+}
+func (a *App) initHandler(s services, authJWT *jwt.JWT, log *zap.Logger) (h http.Handler, err error) {
+	// requests handler
+	reqH, err := handler.New(
+		s.users,
+		s.nodes,
+		s.subscr,
+		s.auth,
+		handler.WithLogger(log))
+	if err != nil {
+		return
+	}
+
+	// security handler
+	secH, err := security.New(authJWT)
+	if err != nil {
+		return
+	}
+
+	// api handler
+	if h, err = api.NewHandler(reqH, secH); err != nil {
+		return
+	}
+
+	return
 }
 
 func (app *App) Run() error {
