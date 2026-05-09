396bda82e247ec38339b5d3b735f72a59b1b46cd
stepka reserva <stepka@reserva.com>
2026-05-09 17:37:47 +0200

    rename - move

--- frontend/admpage/openapi-ts.config.js
+++ frontend/admpage/openapi-ts.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "@hey-api/openapi-ts";
 
 export default defineConfig({
-  input: "../../../pkg/api/http/openapi/openapi.yaml",
+  input: "../../backend/nodeman/pkg/api/http/openapi/openapi.yaml",
   output: "./src/services/api/generated",
   plugins: ["@hey-api/typescript", "@hey-api/sdk", "@hey-api/client-fetch"],
   parser: {
     filters: {
       tags: {
-        include: ["userpage"],
+        include: ["admpage"],
       },
     },
   },
