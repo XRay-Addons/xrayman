762cde45478adb133a33a7936af25e2da2450b40
stepka reserva <stepka@reserva.com>
2026-05-12 12:28:29 +0200

    Urgent: fix localhost url

--- frontend/admpage/src/services/api/client-setup.ts
+++ frontend/admpage/src/services/api/client-setup.ts
@@ -6,7 +6,7 @@ import { config } from "@/config/config";
 export function setupClient() {
   client.setConfig({
     auth: getToken,
-    baseUrl: "http://localhost:1001/api", //config.ApiPrefix,
+    baseUrl: config.ApiPrefix,
     fetch: authFetch,
   });
 }
