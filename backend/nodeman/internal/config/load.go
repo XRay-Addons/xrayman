6623c389b65057d220d0fc7a0ed9097f2c747c84
stepka reserva <stepka@reserva.com>
2026-05-14 22:20:32 +0200

    remove extra config param

--- backend/nodeman/internal/config/load.go
+++ backend/nodeman/internal/config/load.go
@@ -3,8 +3,6 @@ package config
 import (
 	"flag"
 	"os"
-	"path"
-	"runtime"
 
 	"github.com/XRay-Addons/xrayman/common/xerr"
 	"github.com/caarlos0/env/v6"
@@ -18,7 +16,6 @@ func LoadConfig() (*Config, error) {
 	if err := readEnvParams(cfg); err != nil {
 		return nil, err
 	}
-	initCertPaths(cfg)
 
 	return cfg, nil
 }
@@ -26,19 +23,9 @@ func LoadConfig() (*Config, error) {
 func defaultConfig() *Config {
 	return &Config{
 		Endpoint:       "localhost:80",
-		certsDir:       defaultCertsDir(),
 		UserSpaPrefix:  "/u",
 		AdminSpaPrefix: "/adm",
 		APIPrefix:      "/api",
-	}
-}
-
-func defaultCertsDir() string {
-	switch runtime.GOOS {
-	case "darwin", "linux":
-		return "/usr/local/bin/xrayman"
-	default:
-		return ""
 	}
 }
 
@@ -47,9 +34,6 @@ func readCLIParams(c *Config) error {
 
 	fs.StringVar(&c.Endpoint, "a", c.Endpoint,
 		"server endpoint tcp address, like :8080, 127.0.0.1:80, localhost:22")
-	fs.StringVar(&c.certsDir, "x", c.certsDir,
-		`nodeman<->node connection encryption certificates dir.
-should contains nodeman.crt nodeman.key ca.crt`)
 	fs.StringVar(&c.DBConn, "db", c.DBConn,
 		`db connection string`)
 	fs.StringVar(&c.UserSpaPrefix, "userspa", c.UserSpaPrefix,
@@ -75,25 +59,4 @@ func readEnvParams(c *Config) error {
 		return xerr.WrapWithStack(err)
 	}
 	return nil
-}
-
-func initCertPaths(c *Config) {
-	{
-		nodemanCrt := path.Join(c.certsDir, "nodeman.crt")
-		if exists, err := checkFileExists(nodemanCrt); err == nil && exists {
-			c.nodemanCrt = nodemanCrt
-		}
-	}
-	{
-		nodemanKey := path.Join(c.certsDir, "nodeman.key")
-		if exists, err := checkFileExists(nodemanKey); err == nil && exists {
-			c.nodemanKey = nodemanKey
-		}
-	}
-	{
-		rootCrt := path.Join(c.certsDir, "ca.crt")
-		if exists, err := checkFileExists(rootCrt); err == nil && exists {
-			c.rootCrt = rootCrt
-		}
-	}
 }
--- backend/nodeman/internal/config/validate.go
+++ backend/nodeman/internal/config/validate.go
@@ -1,21 +1,15 @@
 package config
 
 import (
-	"fmt"
 	"net"
-	"os"
 	"strings"
 
 	"github.com/XRay-Addons/xrayman/common/xerr"
-	"github.com/XRay-Addons/xrayman/nodeman/internal/errdefs"
 )
 
 func Validate(c Config) error {
 	if _, err := net.ResolveTCPAddr("tcp", c.Endpoint); err != nil {
 		return xerr.New("invalid endpoint", xerr.Withf("endpoint: %s", c.Endpoint))
-	}
-	if err := checkCerts(c); err != nil {
-		return err
 	}
 	if err := checkDBConn(c); err != nil {
 		return err
@@ -28,42 +22,6 @@ func Validate(c Config) error {
 	}
 
 	return nil
-}
-
-func checkFileExists(path string) (bool, error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		return false, nil
-	}
-	if !info.Mode().IsRegular() {
-		return false, xerr.New("file is not regular",
-			errdefs.WithFile(path))
-	}
-	return true, nil
-}
-
-func checkCerts(c Config) error {
-	// all 3 certs should exists or not together
-	certs := []string{c.nodemanCrt, c.nodemanKey, c.rootCrt}
-	existsCount := 0
-	existsDescription := ""
-	for _, f := range []string{c.nodemanCrt, c.nodemanKey, c.rootCrt} {
-		exists, err := checkFileExists(f)
-		if err != nil {
-			return err
-		}
-		existsDescription += fmt.Sprintf("cert %s: %v", f, exists)
-		if exists {
-			existsCount++
-		}
-	}
-
-	if existsCount == 0 || existsCount == len(certs) {
-		return nil
-	}
-
-	return xerr.New("cert files inconsistency",
-		xerr.Withf("inconsistency: %s", existsDescription))
 }
 
 func checkDBConn(c Config) error {
