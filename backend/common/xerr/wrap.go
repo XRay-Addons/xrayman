9765e7c246116551384bbfe8d9b178a03b8bc4df
stepka reserva <stepka@reserva.com>
2026-05-15 10:16:25 +0200

    fix wrapping error

--- backend/common/xerr/wrap.go
+++ backend/common/xerr/wrap.go
@@ -10,6 +10,12 @@ func Wrap(base error, opts ...Option) error {
 	var err *xerror
 	if !errors.As(base, &err) {
 		err = &xerror{err: base}
+	} else {
+		err = &xerror{
+			err:   base,
+			with:  append([]string{}, err.with...),
+			stack: append([]string{}, err.stack...),
+		}
 	}
 
 	for _, o := range opts {
