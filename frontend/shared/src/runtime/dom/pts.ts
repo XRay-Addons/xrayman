1afd5f266a33135ef194a32e2b80ddc0363c3b9e
stepka reserva <stepka@reserva.com>
2026-07-16 14:56:55 +0200

    better tags

--- frontend/shared/src/runtime/dom/pts.ts
+++ frontend/shared/src/runtime/dom/pts.ts
@@ -1,20 +1,31 @@
 // methods for safe and type-safe access to element parts.
 // use attr data-pt="name" to access via pt<ElemType>(el, name)
-// use attr data-pt-private="name"  to access via pt<ElemType>(el, name, "private")
+// use attr data-item-name  to access via item<ElemType>(el, "item-name")
 
 // get element by data-part attribute
-export function pt<T extends Element>(el: ParentNode, name: string, suffix: string = ""): T {
-  const attr = suffix === "" ? "data-pt" : `data-pt-${suffix}`;
-  const pt = el.querySelector(`[${attr}="${name}"]`);
+export function pt<T extends Element>(el: ParentNode, name: string): T {
+  const pt = el.querySelector(`[data-pt="${name}"]`);
   if (!pt) {
-    throw new Error(`<${el}> missing ${attr}="${name}"`);
+    throw new Error(`<${el}> missing data-pt="${name}"`);
   }
   return pt as T;
 }
 
 // get elements by data-part attribute
-export function pts<T extends Element>(el: ParentNode, name: string, suffix: string = ""): T[] {
-  const attr = suffix === "" ? "data-pt" : `data-pt-${suffix}`;
-  const elements = el.querySelectorAll(`[${attr}="${name}"]`);
+export function pts<T extends Element>(el: ParentNode, name: string): T[] {
+  const elements = el.querySelectorAll(`[data-pt="${name}"]`);
   return Array.from(elements) as T[];
+}
+
+export function item<T extends Element>(el: ParentNode, name: string): T {
+  const item = el.querySelector(`[data-${name}]`);
+  if (!item) {
+    throw new Error(`<${el}> missing data-${name}`);
+  }
+  return item as T;
+}
+
+export function items<T extends Element>(el: ParentNode, name: string): T[] {
+  const items = el.querySelectorAll(`[data-${name}]`);
+  return Array.from(items) as T[];
 }
