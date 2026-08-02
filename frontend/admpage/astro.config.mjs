a022452f2a3af1829f0da4a08ce748dd793194ed
stepka reserva <stepka@reserva.com>
2026-08-03 00:26:29 +0200

    fix pages

--- frontend/admpage/astro.config.mjs
+++ frontend/admpage/astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     relativeLinks(),
     purgecss(),
     compress({
-      JavaScript: true,
+      JavaScript: false,
       CSS: true,
       HTML: true,
     }),
