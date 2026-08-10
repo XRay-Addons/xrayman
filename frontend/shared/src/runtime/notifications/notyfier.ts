e1529dc0b65bbf7623de7c7fdc0931613ddcf008
stepka reserva <stepka@reserva.com>
2026-08-10 15:44:16 +0200

    fix notyf on opened dialogs

--- frontend/shared/src/runtime/notifications/notyfier.ts
+++ frontend/shared/src/runtime/notifications/notyfier.ts
@@ -12,8 +12,27 @@ export class Notyfier {
   }
 
   errorNotification(message: string, details?: string) {
+    this._prepareContainer();
     const error = details ? `<b>${message}</b><br/>${details}` : `<b>${message}</b>`;
     this.notyf!.error({ message: error });
     console.log("error:", message, details);
+  }
+
+  _prepareContainer() {
+    const notyfContainer = document.querySelector(".notyf");
+    if (!notyfContainer) return;
+
+    // attach notyf container to open modal dialog if opened
+    const openDialogs = Array.from(document.querySelectorAll("dialog[open]"));
+    if (openDialogs.length > 0) {
+      const activeDialog = openDialogs[openDialogs.length - 1];
+      if (notyfContainer.parentElement !== activeDialog) {
+        activeDialog.appendChild(notyfContainer);
+      }
+    } else {
+      if (notyfContainer.parentElement !== document.body) {
+        document.body.appendChild(notyfContainer);
+      }
+    }
   }
 }
