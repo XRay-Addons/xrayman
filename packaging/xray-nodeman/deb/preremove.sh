5f43073eff93649ad445cd54143ad3d0be58ed79
stepka reserva <stepka@reserva.com>
2026-09-18 12:27:13 +0200

    fix scripts

---
+++ packaging/xray-nodeman/deb/preremove.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+SERVICE="xray-nodeman"
+
+if [ "$1" = "remove" ]; then
+    echo "Run $SERVICE package preremove.sh script..."
+
+    if systemctl is-active --quiet $SERVICE.service; then
+        echo "Stopping $SERVICE service"
+        systemctl stop $SERVICE.service
+    else
+        echo "$SERVICE service is already stopped, skipping"
+    fi
+
+    if systemctl is-enabled --quiet "$SERVICE.service"; then
+        echo "Disabling $SERVICE service"
+        systemctl disable $SERVICE.service
+    else
+        echo "$SERVICE service is already disabled, skipping"
+    fi
+fi
+
+exit 0
