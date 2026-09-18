c0711c48b487d823da98731412e6983e3dc952d0
stepka reserva <stepka@reserva.com>
2026-09-18 13:01:34 +0200

    fix comma

--- packaging/xray-nodeman/deb/postinstall.sh
+++ packaging/xray-nodeman/deb/postinstall.sh
@@ -16,14 +16,14 @@ fi
 if ! getent group $SERVICE_GROUP >/dev/null; then
     echo "Create system group '$SERVICE_GROUP'"
     groupadd --system "$SERVICE_GROUP"
-else:
+else
      echo "System group '$SERVICE_GROUP' already exists, skipping"
 fi
 
 if ! getent passwd "$SERVICE_USER" >/dev/null 2>&1; then
     echo "Create system user '$SERVICE_USER'"
     useradd --system --gid "$SERVICE_GROUP" --no-create-home --shell /usr/sbin/nologin "$SERVICE_USER"
-else:
+else
     echo "System user '$SERVICE_USER' already exists, skipping"
 fi
 
