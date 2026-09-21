9b2f5b941a3f21aaf6e0a3408ca0ed0d6c2baf39
stepka reserva <stepka@reserva.com>
2026-09-21 15:29:03 +0200

    remove common code

--- packaging/xray-nodeman/docker/publish.sh
+++ packaging/xray-nodeman/docker/publish.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-. ./packaging/common/utils.sh
-
 set -e
+
+copy() {
+    mkdir -p "$(dirname "$2")"
+    cp -r "$1" "$2"
+    if [[ -n "$3" ]]; then
+        chmod "$3" "$2"
+    fi
+}
+
+replace() {
+    local src="$1"
+    local dst="$2"
+    shift 2
+
+    local args=()
+    while (($#)); do
+        args+=("-e" "s/$1/$2/")
+        shift 2
+    done
+
+    mkdir -p "$(dirname "$dst")"
+    sed "${args[@]}" "$src" > "$dst"
+}
+
 
 CLEAN_VER="${1:-0.0.0}"
 
