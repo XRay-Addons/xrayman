c058ce71c0b1c0a41c321e8906a016637c347a1d
stepka reserva <stepka@reserva.com>
2026-09-21 15:47:39 +0200

    fix remove

--- packaging/xray-node/deb/preremove.sh
+++ packaging/xray-node/deb/preremove.sh
@@ -21,8 +21,21 @@ removeSystemdService() {
     fi
 }
 
+removeNonPackageData() {
+    local SERVICE="$1"
+    local PERSISTENT="/var/lib/$SERVICE/persistent"
+
+    if [ -d "$DIR" ]; then
+        echo "Removing $SERVICE persistent data from '$PERSISTENT'"
+        find "$PERSISTENT" -mindepth 1 -delete # only content, not dir itself
+    else
+        echo "No persistent data dir '$PERSISTENT', nothing to remove"
+    fi
+}
+
 if [ "$1" = "remove" ]; then
     echo "Run $SERVICE package preremove.sh script..."
     removeSystemdService $SERVICE
+    removeNonPackageData $SERVICE
 fi
 exit 0
