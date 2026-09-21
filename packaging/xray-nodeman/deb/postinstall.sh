102ee3631c97a978cf58ad5732ba10191785054a
stepka reserva <stepka@reserva.com>
2026-09-21 10:45:20 +0200

    packaging/xray-nodeman/deb/preinstall.sh

--- packaging/xray-nodeman/deb/postinstall.sh
+++ packaging/xray-nodeman/deb/postinstall.sh
@@ -13,20 +13,6 @@ if [ -f "$DPKG_GARBAGE_FILE" ]; then
     rm -f "$DPKG_GARBAGE_FILE"
 fi
 
-if ! getent group $SERVICE_GROUP >/dev/null; then
-    echo "Create system group '$SERVICE_GROUP'"
-    groupadd --system "$SERVICE_GROUP"
-else
-     echo "System group '$SERVICE_GROUP' already exists, skipping"
-fi
-
-if ! getent passwd "$SERVICE_USER" >/dev/null 2>&1; then
-    echo "Create system user '$SERVICE_USER'"
-    useradd --system --gid "$SERVICE_GROUP" --no-create-home --shell /usr/sbin/nologin "$SERVICE_USER"
-else
-    echo "System user '$SERVICE_USER' already exists, skipping"
-fi
-
 echo "Reload systemd units."
 systemctl daemon-reload
 
