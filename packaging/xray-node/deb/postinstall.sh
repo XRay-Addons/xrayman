5da8febd48135874a06a6e529bb37586ea529659
stepka reserva <stepka@reserva.com>
2026-09-21 14:37:28 +0200

    common deb scripts code

--- packaging/xray-node/deb/postinstall.sh
+++ packaging/xray-node/deb/postinstall.sh
@@ -1,50 +1,12 @@
 #!/bin/sh
 set -e
 
+. /usr/lib/xray-node/utils.sh
+
 SERVICE="xray-node"
 SERVICE_USER="xray-node"
 SERVICE_GROUP="xray-node"
 
 echo "Run $SERVICE package postinstall.sh script..."
-
-DPKG_GARBAGE_FILE="/etc/$SERVICE/$SERVICE.env.dpkg-dist"
-if [ -f "$DPKG_GARBAGE_FILE" ]; then
-    echo "Removing dpkg garbage '$DPKG_GARBAGE_FILE'"
-    rm -f "$DPKG_GARBAGE_FILE"
-fi
-
-if ! getent group $SERVICE_GROUP >/dev/null; then
-    echo "Create system group '$SERVICE_GROUP'"
-    groupadd --system "$SERVICE_GROUP"
-else
-     echo "System group '$SERVICE_GROUP' already exists, skipping"
-fi
-
-if ! getent passwd "$SERVICE_USER" >/dev/null 2>&1; then
-    echo "Create system user '$SERVICE_USER'"
-    useradd --system --gid "$SERVICE_GROUP" --no-create-home --shell /usr/sbin/nologin "$SERVICE_USER"
-else
-    echo "System user '$SERVICE_USER' already exists, skipping"
-fi
-
-echo "Reload systemd units."
-systemctl daemon-reload
-
-echo "Enable $SERVICE service."
-systemctl enable $SERVICE
-
-
-printf '\n'
-printf '╔════════════════════════════════════════════════════════════════╗\n'
-printf '║     XRayMan service successfully installed: %-16s   ║\n' "$SERVICE"
-printf '╠════════════════════════════════════════════════════════════════╣\n'
-printf '║ [!] Setup app configuration [!]                                ║\n'
-printf '║   [!] User manual available via sudo apt show %-16s ║\n' "$SERVICE"
-printf '║                                                                ║\n'
-printf '║ Then start/restart the service:                                ║\n'
-printf '║   sudo systemctl restart %-16s                      ║\n' "$SERVICE"
-printf '║                                                                ║\n'
-printf '╚════════════════════════════════════════════════════════════════╝\n'
-printf '\n'
-
+installSystemdService "$SERVICE"
 exit 0
--- packaging/xray-node/deb/postremove.sh
+++ packaging/xray-node/deb/postremove.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 set -e
 
+. /usr/lib/xray-node/utils.sh
+
 SERVICE="xray-node"
 SERVICE_USER="xray-node"
 SERVICE_GROUP="xray-node"
 
 if [ "$1" = "purge" ]; then
     echo "Run $SERVICE package postremove.sh script..."
-
-    if getent passwd "$SERVICE_USER" >/dev/null 2>&1; then
-        echo "Removing system user '$SERVICE_USER'"
-        userdel "$SERVICE_USER"
-    else
-        echo "System user '$SERVICE_USER' does not exist, skipping"
-    fi
-
-    if getent group "$SERVICE_GROUP" >/dev/null 2>&1; then
-        echo "Removing system group '$SERVICE_GROUP'"
-        groupdel "$SERVICE_GROUP"
-    else
-        echo "System group '$SERVICE_GROUP' does not exist, skipping"
-    fi
+    removeServiceUser "$SERVICE_USER" "$SERVICE_GROUP"
 fi
 
 exit 0
